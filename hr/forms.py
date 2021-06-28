5d1e9a6c7f9d5e0c6ba99a4c110bd5559f5a9472
Rupa-421 <64897164+Rupa-421@users.noreply.github.com>
2021-06-28 23:25:19 +0530

    fixing bugs

--- hr/forms.py
+++ hr/forms.py
@@ -30,5 +30,5 @@ class PersonForm(forms.ModelForm):
             except (ValueError, TypeError):
                 pass  # invalid input from the client; ignore and fallback to empty City queryset
         elif self.instance.pk:
-            self.fields['vanue'].queryset = self.instance.country.city.vanue_set.order_by('name')
-            #self.fields['vanue'].queryset = self.instance.city.vanue_set.order_by('name')
+            #self.fields['vanue'].queryset = self.instance.country.city.vanue_set.order_by('name')
+            self.fields['vanue'].queryset = self.instance.city.vanue_set.order_by('name')
